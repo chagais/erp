da5c0a401203b73f9cb638567893ecb0cb1354ee
Tiago Oliveira <gsfogo@gmail.com>
2022-09-21 17:48:20 -0300

    ver1.1.1

--- app/views/cabecalho.php
+++ app/views/cabecalho.php
@@ -1,1 +1,12 @@
-<h1>Aqui fica o cabeçalho</h1>
+<div class="topo d-flex justify-content-space-between">
+	<a href="" class="mobmenu alt"><i class="fas fa-bars"></i></a>
+	<a href="index.html" class="logo"><img src="<?php echo URL_BASE ?>assets/img/logo-laravel.png" class="d-inline-block" width="170"></a>
+	
+	<ul class="menu-topo">
+		<li><a href="#" class="text-uppercase"><i class="fas fa-lock"></i> Login</a></li>
+		<li>
+			<img src="<?php echo URL_BASE ?>assets/img/foto01.jpg" class="img-user">
+			<span class="text-dark">Tiago Oliveira</span>
+		</li>
+	</ul>
+</div>	
